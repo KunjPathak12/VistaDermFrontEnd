2b9d8686616cea87eb321112da863a570b180704
KunjPathak12 <kunjpathak1212@gmail.com>
2023-02-06 00:46:44 +0530

    Updates

---
+++ precache-manifest.397bfbdb64a7895effae1b83550ff7d9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd8cc282beccfac397",
+    "url": "/vistadermInternship/static/css/main.9ee096a1.chunk.css"
+  },
+  {
+    "revision": "a3bd8cc282beccfac397",
+    "url": "/vistadermInternship/static/js/main.a3bd8cc2.chunk.js"
+  },
+  {
+    "revision": "a28c07959e87d5f1d8de",
+    "url": "/vistadermInternship/static/js/1.a28c0795.chunk.js"
+  },
+  {
+    "revision": "5ff409295976c245f0f1",
+    "url": "/vistadermInternship/static/js/runtime~main.5ff40929.js"
+  },
+  {
+    "revision": "a63bb8910985a63655e0d8c82acb9c55",
+    "url": "/vistadermInternship/static/media/svg-1.a63bb891.svg"
+  },
+  {
+    "revision": "6ad25628b7554e25b09faf69bddc22df",
+    "url": "/vistadermInternship/static/media/video.6ad25628.mp4"
+  },
+  {
+    "revision": "9a3dab685ffcd5be5ef133bed1feeda7",
+    "url": "/vistadermInternship/static/media/svg-3.9a3dab68.svg"
+  },
+  {
+    "revision": "b662e7d6731d6054f8eba99c5aa3298f",
+    "url": "/vistadermInternship/static/media/svg-6.b662e7d6.svg"
+  },
+  {
+    "revision": "a0874f2027f3b39ccd9784dd108b54a3",
+    "url": "/vistadermInternship/static/media/svg-7.a0874f20.svg"
+  },
+  {
+    "revision": "c2fd9a4b8ae9e4677597273b67a6bca1",
+    "url": "/vistadermInternship/static/media/svg-2.c2fd9a4b.svg"
+  },
+  {
+    "revision": "a45446b77b8e09f0ad6c42b122e3346b",
+    "url": "/vistadermInternship/static/media/svg-4.a45446b7.svg"
+  },
+  {
+    "revision": "61c6ceafe25b574385eeaa8e1d145050",
+    "url": "/vistadermInternship/static/media/svg-5.61c6ceaf.svg"
+  },
+  {
+    "revision": "c804db0115dfc4ca22d42adc6605239e",
+    "url": "/vistadermInternship/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be13726aaf39f541faa47bb08261f26b.js"
+  "/vistadermInternship/precache-manifest.397bfbdb64a7895effae1b83550ff7d9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/vistadermInternship/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
