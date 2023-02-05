371c9e3a5fbf4ba68b997a462e359a67be05ae2b
KunjPathak12 <kunjpathak1212@gmail.com>
2023-02-06 01:05:34 +0530

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import BrowserRouter from 'react-router-dom/BrowserRouter'
+// import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 // ReactDOM.render((
 //    <BrowserRouter basename={process.env.PUBLIC_URL}>
 //      <App />
 //    </BrowserRouter>
-// ), ...);
+// ), ...)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
